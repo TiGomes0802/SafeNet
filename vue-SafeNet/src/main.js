1eb7ee5fb1fb175646c2b4169ed9f9d87942d0d5
2231804 <2231804@my.ipleiria.pt>
2025-04-17 00:01:52 +0100

    Raiz do projeto

--- vue-SafeNet/src/main.js
+++ vue-SafeNet/src/main.js
@@ -2,13 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
+
+//import ErrorMessage from './components/common/ErrorMessage.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+const apiDomain = import.meta.env.VITE_API_DOMAIN
+
+// Default Axios configuration
+axios.defaults.baseURL = `http://${apiDomain}/api`
+
+
+app.provide('serverBaseUrl', apiDomain)
+
 app.mount('#app')
+
