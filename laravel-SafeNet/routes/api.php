67de06977467e77a11f5ffbcb88daf43470dc23d
Daniel Brás <danielbras04@gmail.com>
2025-04-29 11:33:06 +0100

    Rotas

--- laravel-SafeNet/routes/api.php
+++ laravel-SafeNet/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\api\JogoController;
 use App\Http\Controllers\api\CursoController;
+use App\Http\Controllers\api\UnidadeController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
@@ -18,6 +19,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('curso/{idCurso}', [CursoController::class, 'show']);
     Route::post('curso', [CursoController::class, 'createCurso']); 
     Route::put('curso/{idCurso}', [CursoController::class, 'update']);
+
+    Route::get('/unidade/{idCurso}', [UnidadeController::class, 'index']);
+    Route::get('/unidade/{idCurso}/{idUnidade}', [UnidadeController::class, 'show']);
+    Route::post('/unidade/{idCurso}', [UnidadeController::class, 'createUnidade']);
+    Route::put('/unidade/{idCurso}/{idUnidade}', [UnidadeController::class, 'update']);
 });
 
 Route::post('/auth/login', [AuthController::class, 'login']);
