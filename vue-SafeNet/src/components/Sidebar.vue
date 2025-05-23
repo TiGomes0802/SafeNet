d82ef6ca7dfe666b866d2d8fcd4e7249db8b2a6d
2231804 <2231804@my.ipleiria.pt>
2025-05-23 15:24:25 +0100

    frontend das missoes

--- vue-SafeNet/src/components/Sidebar.vue
+++ vue-SafeNet/src/components/Sidebar.vue
@@ -46,7 +46,7 @@ const handleLinkClick = () => {
 
     <!-- Mini sidebar para Responsividade-->
     <aside v-if="!isOpen && windowWidth < 768"
-        class="fixed inset-y-0 left-0 w-12 bg-white shadow-md z-40 flex flex-col items-center pt-4 md:hidden">
+        class="inset-y-0 left-0  w-12 bg-white shadow-md z-40 flex flex-col items-center pt-4 md:hidden">
         <button @click="toggleSidebar" class="text-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
@@ -60,11 +60,11 @@ const handleLinkClick = () => {
         <aside v-if="isOpen || windowWidth >= 768"
             class="fixed md:static inset-y-0 z-40 w-64 bg-white shadow-md p-4 flex flex-col justify-between transform transition-transform duration-300 md:translate-x-0"
             :class="[
-                { '-translate-x-full': !isOpen && windowWidth < 768 },
+                { '-translate-x-full ': !isOpen && windowWidth < 768 },
                 { 'left-0': isOpen || windowWidth >= 768 }
             ]">
             <!--Botão X-->
-            <button @click="toggleSidebar" class="absolute top-4 right-4 md:hidden text-gray-600 hover:text-gray-800">
+            <button @click="toggleSidebar" class="absolute top-4 right-4 md:hidden text-gray-600 hover:text-gray-800 ">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -77,15 +77,11 @@ const handleLinkClick = () => {
                     <div class="mb-6">
                         <h2 class="text-sm font-semibold text-gray-500 mb-2 px-3">Cursos</h2>
                         <nav class="space-y-2">
-                            <router-link
-                                v-for="curso in storeCurso.cursos"
-                                :key="curso.id"
-                                :to="curso.estado !== 0 ? `/curso/${curso.id}` : ''"
-                                class="block py-2 px-6 rounded"
+                            <router-link v-for="curso in storeCurso.cursos" :key="curso.id"
+                                :to="curso.estado !== 0 ? `/curso/${curso.id}` : ''" class="block py-2 px-6 rounded"
                                 :class="curso.estado === 0 ? 'bg-gray-200 text-gray-400 cursor-not-allowed' : 'hover:bg-gray-100 text-black'"
-                                @click="curso.estado === 0 ? $event.preventDefault() : handleLinkClick"
-                                >
-                                    {{ curso.nome }}
+                                @click="curso.estado === 0 ? $event.preventDefault() : handleLinkClick">
+                                {{ curso.nome }}
                             </router-link>
                         </nav>
                     </div>
--- vue-SafeNet/src/components/missoes/Missoes.vue
+++ vue-SafeNet/src/components/missoes/Missoes.vue
@@ -11,7 +11,7 @@
     
     <div class="flex h-screen">
         <Sidebar :isOpen="isSidebarOpen" @toggle="isSidebarOpen = !isSidebarOpen" />
-        <div :class="['flex-1 bg-gray-100 p-6 overflow-y-scroll transition-all duration-300', dynamicPadding]">
+        <!--<div :class="['flex-1 bg-gray-100 p-6 overflow-y-scroll transition-all duration-300', dynamicPadding]">
             <div class="flex flex-col gap-4">
                 <p class="text-3xl ">Missões</p>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -33,6 +33,49 @@
                     <div v-for="conquista in storeMissao.conquistas" :key="conquista.id" class="bg-white p-4 rounded shadow">
                         <h3 class="text-xl font-semibold">{{ conquista.missao.descricao }}</h3>
                         <p>{{ conquista.progresso }} / {{ conquista.missao.objetivo }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>-->
+        <div class="flex flex-col items-center justify-start w-full mx-auto bg-gray-100">
+            <div class="m-5 md:m-10 lg:m-[60px] w-[65%]">
+                <div class="flex flex-col items-center justify-between w-full mx-auto mb-10">
+                    <div class="flex flex-col sm:flex-row items-center justify-between w-full mb-4">
+                        <p class="text-4xl">Missões Diárias</p>
+                        <p class="text-2xl justify-end text-yellow-400">5 Horas ⏰</p>
+                    </div>
+                    <div class="flex flex-col border-3 border-gray-600 rounded-lg bg-white p-2 w-full">
+                        <div v-for="missao in storeMissao.missoes" :key="missao.id" class="flex flex-col items-center w-full my-5">
+                            <p class="text-xl font-semibold text-center w-2/6 mb-4">{{ missao.missao.descricao }}</p>
+                            <div class="relative flex flex-row w-[93%] bg-gray-200 rounded-full h-6 mb-2">
+                                <div class="bg-green-500 h-6 rounded-full transition-all duration-300"
+                                    :style="{ width: ((missao.progresso / missao.missao.objetivo) * 100) + '%' }">
+                                </div>
+                                <p class="absolute inset-0 flex items-center justify-center text-sm text-gray-600 font-medium select-none">
+                                    {{ missao.progresso }} / {{ missao.missao.objetivo }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col items-center justify-between w-full mx-auto">
+                    <div class="flex flex-col w-full mb-4">
+                        <p class="text-4xl text-center">Conquistas</p>
+                    </div>
+                    <div class="flex flex-col border-3 border-gray-600 rounded-lg bg-white p-2 w-full">
+                        <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full">
+                            <div v-for="conquista in storeMissao.conquistas" :key="conquista.id" class="flex flex-col items-center w-full my-5 border-2 border-gray-600 rounded-lg bg-white p-2">
+                            <h3 class="text-md font-semibold text-center mb-4">{{ conquista.missao.descricao }}</h3>
+                            <div class="relative flex flex-row w-[93%] bg-gray-200 rounded-full h-6">
+                                <div class="bg-green-500 h-6 rounded-full transition-all duration-300"
+                                :style="{ width: ((conquista.progresso / conquista.missao.objetivo) * 100) + '%' }">
+                                </div>
+                                <p class="absolute inset-0 flex items-center justify-center text-sm text-gray-600 font-medium select-none">
+                                {{ conquista.progresso }} / {{ conquista.missao.objetivo }}
+                                </p>
+                            </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
